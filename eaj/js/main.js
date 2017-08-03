0d7fcbd560d146c715b0a1ad56889228f4a84c96
xiaonainiu <es930119@gmail.com>
2017-08-04 00:12:47 +1000

    update

---
+++ eaj/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ES on 2017/8/3.
+ */
+
+//x=document.getElementById("intro");
+//document.write('<p>id="intro" 来点中文' + x.innerHTML + '</p>');
+
+function moveonButton(obj){
+    obj.style.color="blue";
+}
+function moveoutButton(obj){
+    obj.style.color="red";
+}
+//
+//x=document.getElementsByClassName("button");
+//onmouseover="x.style.backgroundColor='red';";
+//onmouseout="x.style.backgroundColor='blue';";
