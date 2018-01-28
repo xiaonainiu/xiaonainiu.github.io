bedab10577872e0c0eb535a269d4d1a4c0cf848f
xiaonainiu <es930119@gmail.com>
2018-01-28 21:41:34 +0800

    fix on ios
    fxxk the ios

--- twoyears/js/twoyears.js
+++ twoyears/js/twoyears.js
@@ -1,5 +1,11 @@
+
 function audioPlay(){
     var audio = $('#mou')[0];
+    audio.play();
+    console.log(1);
+}
+function bgmPlay(){
+    var audio = $('#bgm')[0];
     audio.play();
     console.log(1);
 }
